986997abb48ad59ddb9b8a775726b96c7009f872
Vladimir <qv.odron00638@student.tsu.edu.ph>
2024-03-03 20:40:14 +0800

    Final Revisions for final personal page

--- Odron.js
+++ Odron.js
@@ -1,1 +1,13 @@
-JS
+
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+setInterval(()=>{
+    let currentTime = new Date();
+    hrs.innerHTML =  currentTime.getHours();
+    min.innerHTML = currentTime.getMinutes();
+    sec.innerHTML = currentTime.getSeconds();
+
+},1000)
+
