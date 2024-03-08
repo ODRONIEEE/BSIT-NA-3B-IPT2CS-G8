ba0ac7b3a400c46c221f31e13a5f78c504549d96
Christian <cj.demetria00803@student.tsu.edu.ph>
2024-03-09 05:40:21 +0800

    Finished Product

--- Demetria.js
+++ Demetria.js
@@ -1,3 +1,11 @@
+// Toggle Visibility Of Navbar When Button Clicked
+
+const navToggle = document.querySelector(".navbar-toggle");
+navToggle.addEventListener("click", function () {
+  document.querySelector(".portfolio-navbar").classList.toggle("show");
+});
+
+
 // Tab Interface For About
 const aboutHeading = document.querySelector(".about-heading");
 const aboutTabs = document.querySelectorAll(".about-tab");
