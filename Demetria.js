bcab65d12f08aabbc55eb1b9a1fc15da18afc115
Christian <cj.demetria00803@student.tsu.edu.ph>
2024-03-08 22:15:57 +0800

    Added Javascript file

--- Demetria.js
+++ Demetria.js
@@ -1,1 +1,53 @@
-js file ni dems
+// Tab Interface For About
+const aboutHeading = document.querySelector(".about-heading");
+const aboutTabs = document.querySelectorAll(".about-tab");
+aboutHeading.onclick = (event) => {
+  event.preventDefault();
+  const clickedItemId = event.target.dataset.id;
+  if (clickedItemId) {
+    aboutHeading.querySelector(".active").classList.remove("active");
+    event.target.classList.add("active");
+
+    aboutTabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    const correspondingTab = document.getElementById(clickedItemId);
+    correspondingTab.classList.add("active");
+  }
+};
+
+// Navbar Header Sticky While Scroll
+function stickyNav() {
+  var headerHeight = document.querySelector("#home").offsetHeight / 2;
+  var navbar = document.querySelector("header");
+  var scrollValue = window.scrollY;
+
+  if (scrollValue > headerHeight) {
+    navbar.classList.add("header-sticky");
+  } else if (scrollValue < headerHeight) {
+    navbar.classList.remove("header-sticky");
+  }
+}
+window.addEventListener("scroll", stickyNav);
+
+// Dark and Light Theme Toggle
+function isLight() {
+  return localStorage.getItem("dark-mode");
+}
+function toggleRootClass() {
+  document.querySelector("body").classList.toggle("dark");
+}
+function toggleLocalStorageItem() {
+  if (isLight()) {
+    localStorage.removeItem("dark-mode");
+  } else {
+    localStorage.setItem("dark-mode", "set");
+  }
+}
+if (isLight()) {
+  toggleRootClass();
+}
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+  toggleLocalStorageItem();
+  toggleRootClass();
+});
